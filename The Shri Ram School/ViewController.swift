b5be269a58164f2002676e0cbc47da13108d4fa0
kabiroberai <oberai.kabir@gmail.com>
2015-08-21 22:02:45 +0530

    Initial Commit

---
+++ The Shri Ram School/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Shri Ram School
+//
+//  Created by Kabir Oberai on 21/08/15.
+//  Copyright (c) 2015 The Shri Ram School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
